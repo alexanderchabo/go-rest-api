17ca5a8072021ebb64dbf860e6cf78b166851caa
Alexander Chabo <alexander.chabo@gmail.com>
2018-06-05 16:40:40 -0400

    Handling some parameters

--- src/main.go
+++ src/main.go
@@ -11,9 +11,25 @@ import (
 func main() {
 	var router = mux.NewRouter()
 	router.HandleFunc("/healthcheck", healthCheck).Methods("GET")
+  router.HandleFunc("/message", handleQryMessage).Methods("GET")
+  router.HandleFunc("/m/{msg}", handleUrlMessage).Methods("GET")
 
 	fmt.Println("Running server!")
 	log.Fatal(http.ListenAndServe(":3000", router))
+}
+
+func handleQryMessage(w http.ResponseWriter, r *http.Request) {
+	vars := r.URL.Query()
+	message := vars.Get("msg")
+
+	json.NewEncoder(w).Encode(map[string]string{"message": message})
+}
+
+func handleUrlMessage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	message := vars["msg"]
+
+	json.NewEncoder(w).Encode(map[string]string{"message": message})
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
