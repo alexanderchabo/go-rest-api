39221e14a34f3aa060e5ce409f59a55d27725bf4
Alexander Chabo <alexander.chabo@gmail.com>
2018-06-06 15:58:28 -0400

    Connected to postgres APP_DB

--- src/main.go
+++ src/main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
 	_ "github.com/lib/pq"
 )
 
-func main() {
+const (
+	dbuser = "postgres"
+	dbpass = ""
+	dbname = "APP_DB"
+)
 
+func main() {
+	dbInit()
 	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func dbInit() {
+	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		dbuser, dbpass, dbname)
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to db!")
+
 }
