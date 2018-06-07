f3bce9d5c2231054d4bf73c7d9d43fb45b5f12a2
Alexander Chabo <alexander.chabo@gmail.com>
2018-06-06 23:26:34 -0400

    Refactoring everything (to make it a bit more readable)

---
+++ src/repo.go
@@ -0,0 +1,59 @@
+package main
+
+import "database/sql"
+
+func (u *user) getUser(db *sql.DB) error {
+	return db.QueryRow("SELECT name, age FROM users WHERE id=$1",
+		u.ID).Scan(&u.Name, &u.Age)
+}
+
+func (u *user) updateUser(db *sql.DB) error {
+	_, err :=
+		db.Exec("UPDATE users SET name=$1, age=$2 WHERE id=$3",
+			u.Name, u.Age, u.ID)
+
+	return err
+}
+
+func (u *user) deleteUser(db *sql.DB) error {
+	_, err := db.Exec("DELETE FROM users WHERE id=$1",
+		u.ID)
+
+	return err
+}
+
+func (u *user) createUser(db *sql.DB) error {
+	err := db.QueryRow(
+		"INSERT INTO users(name, age) VALUES($1, $2) RETURNING id",
+		u.Name, u.Age).Scan(&u.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getUsers(db *sql.DB, start, count int) ([]user, error) {
+	rows, err := db.Query(
+		"SELECT id, name, age FROM users LIMIT $1 OFFSET $2",
+		count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	users := []user{}
+
+	for rows.Next() {
+		var u user
+		if err := rows.Scan(&u.ID, &u.Name, &u.Age); err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+	}
+
+	return users, nil
+}
