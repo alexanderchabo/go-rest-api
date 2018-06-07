5be673e787daa940e6df179cb471b927cc865bea
Alexander Chabo <alexander.chabo@gmail.com>
2018-06-06 22:24:03 -0400

    Added a CRUD interface with related tests

--- src/main.go
+++ src/main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
-
-	_ "github.com/lib/pq"
-)
-
 const (
 	dbuser = "postgres"
 	dbpass = ""
@@ -16,25 +7,7 @@ const (
 )
 
 func main() {
-	dbInit()
-	router := NewRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func dbInit() {
-	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		dbuser, dbpass, dbname)
-	db, err := sql.Open("postgres", connectionString)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully connected to db!")
-
+	a := App{}
+	a.Initialize(dbuser, dbpass, dbname)
+	a.Run(":8080")
 }
